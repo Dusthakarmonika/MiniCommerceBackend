e9c24540a2b960d60c9716747df30cc4c8de8541
Monika218 <monikadusthakar3@gmail.com>
2026-06-15 19:21:31 +0530

    create productService with add and display method

--- src/services/ProductServices.java
+++ src/services/ProductServices.java
@@ -1,4 +1,21 @@
 package services;
 
+import model.product;
+
+import java.util.ArrayList;
+
 public class ProductServices {
+    ArrayList<product> list = new ArrayList<>();
+
+    public void addProduct(product Product ){
+        list.add(Product);
+    }
+    public void displayProducts(){
+        if(list.isEmpty()) {
+            System.out.println("No product is added");
+        }
+        for(product p : list){
+            System.out.println(p);
+        }
+    }
 }
