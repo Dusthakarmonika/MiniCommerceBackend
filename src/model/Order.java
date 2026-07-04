378c59e7542e429f850bb971f157ae0497ec6cdd
Monika218 <monikadusthakar3@gmail.com>
2026-07-04 19:18:19 +0530

    Design the order model

---
+++ src/model/Order.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Order {
+    private int orderId;
+    private Customer customer;
+    private ArrayList<CartItem> items;
+    private double totalAmount;
+
+    public Order(int orderId, Customer customer, ArrayList<CartItem> items, double totalAmount){
+        this.orderId = orderId;
+        this.customer = customer;
+        this.items = items;
+        this.totalAmount = totalAmount;
+    }
+    public int getOrderId(){
+        return orderId;
+    }
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+    public Customer getCustomer() {
+        return customer;
+    }
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+    public ArrayList<CartItem> getItems() {
+        return items;
+    }
+    public void setItems(ArrayList<CartItem> items) {
+        this.items = items;
+    }
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+    public String toString(){
+        return  "Order ID : " + orderId +
+                "\n Customer : " + customer +
+                "\n Items : " + items +
+                "\n Total : " + totalAmount;
+
+    }
+}
