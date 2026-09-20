cf46f2a8b89efc975ad3f8fb7f5eac3619fb52eb
Dusthakarmonika <131333030+Dusthakarmonika@users.noreply.github.com>
2026-09-20 05:38:09 +0000

    Add Spring Boot REST API and MySQL configuration

--- src/com/dusthakarmonika/minicommerce/controller/ProductController.java
+++ src/com/dusthakarmonika/minicommerce/controller/ProductController.java
@@ -7,6 +7,12 @@ import com.dusthakarmonika.minicommerce.services.ProductServices;
 import java.util.ArrayList;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.dusthakarmonika.minicommerce.Exception.ProductNotFoundException;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 public class ProductController{
@@ -20,8 +26,32 @@ public class ProductController{
         return productServices.getProduct();
     }
     @PostMapping("/products")
-    public String addProduct(@RequestBody product Product){
+    public ResponseEntity<String> addProduct(@RequestBody product Product){
         productServices.addProduct(Product);
-        return "Product added successfully";
+        return ResponseEntity.status(HttpStatus.CREATED).body("Product added successfully");
     }
+   @GetMapping("/products/{name}")
+public product searchProduct(@PathVariable String name) throws ProductNotFoundException {
+    return productServices.searchProduct(name);
+}
+@PutMapping("/products/{id}")
+
+public ResponseEntity<String> updateStock(@PathVariable("id") int productId, @RequestBody product Product){
+     if(productServices.updateProduct(productId, Product.getStock())){;
+     return ResponseEntity.ok("Stock updated Successfully");
+     }
+     else{
+        return ResponseEntity.notFound().build();
+     }
+}
+@DeleteMapping("/products/{id}")
+public ResponseEntity<String> removeProduct(@PathVariable int id){
+    if(productServices.removeProduct(id)){
+        return ResponseEntity.ok("Product removed successfully");
+    }
+    else{
+        return ResponseEntity.notFound().build();
+    }
+}
+
 }
